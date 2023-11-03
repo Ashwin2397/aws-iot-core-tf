e4f496d41cb27db184b91e5d93114646169eb293
Ashwin2397 <ashwin.selvakumar@lendesk.com>
2023-11-02 18:55:59 -0700

    Add empty controller

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,2 +1,9 @@
 class StatisticsController < ApplicationController
+  # This controller will do the following:
+  # - Receive MM Data
+  # - Perform benchmark testing
+  # - Calculate relevant statistics
+  # - Persist CSV file with statistics to local
+  # - Persist video file to local 
+  def video; end
 end
