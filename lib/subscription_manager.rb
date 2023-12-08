d00a875d2290ec7cc53cf775ddc50a2cdacd2df9
Ashwin2397 <ashwin.selvakumar@lendesk.com>
2023-12-07 22:26:48 -0800

    Slight refactor

--- lib/subscription_manager.rb
+++ lib/subscription_manager.rb
@@ -3,13 +3,14 @@ require_relative 'iot_client'
 class SubscriptionManager
   def initialize(topic, options)
     @topic = topic
-    @count, @log_id = options
+    @count = options.first.to_i
+    @log_id = options[1]
   end
 
   def subscribe
     threads = []
     unsuccessful_subscribers = 0
-    (1..@count.to_i).each do |i|
+    (1..@count).each do |i|
       threads << Thread.new do
         iot_client = IoTClient.new(log_id: @log_id, client_id: i)
         iot_client.subscribe(@topic)
@@ -21,7 +22,7 @@ class SubscriptionManager
 
     # Wait till all subscribers are connected
     sleep(5)
-    puts "#{@count.to_i - unsuccessful_subscribers}/#{@count} subscribers listening ..."
+    puts "#{@count - unsuccessful_subscribers}/#{@count} subscribers listening ..."
 
     threads.each(&:join)
   end
